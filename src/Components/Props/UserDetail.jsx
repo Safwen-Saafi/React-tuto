e9cb4badd200e089368abe835b1ce96279250a62
Safwen-Saafi <saafi.saf0@gmail.com>
2024-11-04 15:46:40 +0100

    findByText query for testing

--- src/Components/Props/UserDetail.jsx
+++ src/Components/Props/UserDetail.jsx
@@ -1,15 +1,28 @@
 import PropTypes from "prop-types";
+import { useEffect, useState } from "react";
 
 export default function UserDetail({ prop }) {
+  const [timerPassed, setTimerPassed] = useState(false);
+  
+  useEffect(() => {
+    setTimeout(() => {
+      setTimerPassed(true);
+    }, 1500);
+  }, []);
+
   return (
-    <div key={prop.id}>
-      <p>ID: {prop.id}</p>
-      <p>Username: {prop.username}</p>
-      <p>Email: {prop.email}</p>
+    <div>
+      {timerPassed && (
+        <>
+          <p>ID: {prop.id}</p>
+          <p>Username: {prop.username}</p>
+          <p>User: {prop.username}</p>
+          <p>Email: {prop.email}</p>
+        </>
+      )}
     </div>
   );
 }
-
 
 UserDetail.propTypes = {
   prop: PropTypes.shape({
--- src/__test__/UserDetail.test.jsx
+++ src/__test__/UserDetail.test.jsx
@@ -1,18 +1,17 @@
-import { describe, it, expect } from 'vitest';
-import { render, screen } from '@testing-library/react';
-import UserDetail from '../Components/Props/UserDetail';
-describe('UserDetail', () => {
-  it('should render user details', () => {
-    // // Create mock props that match the PropTypes requirements
+import { describe, it, expect } from "vitest";
+import { render, screen, configure } from "@testing-library/react";
+import UserDetail from "../Components/Props/UserDetail";
+
+describe("UserDetail", () => {
+  configure({ asyncUtilTimeout: 3000 }); // To make all queries take 3 second min
+  it("should render user details", async () => {
     const mockProp = {
       id: 1,
       username: "testuser",
-      email: "test@example.com"
+      email: "test@example.com",
     };
 
-    render(<UserDetail prop={mockProp}/>);
-    // expect(result.container).toMatchSnapshot();
-    expect(screen.getByText('Username: testuser')).toBeInTheDocument();
-  
+    render(<UserDetail prop={mockProp} />);
+    expect(await screen.findByText(/Username: testuser/)).toBeInTheDocument();
   });
 });
